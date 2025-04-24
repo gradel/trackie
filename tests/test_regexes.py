ab3c5772e165594de43e157f8f2aa4ea8cc7032e
gradel <gerald@hien-online.de>
2025-04-24 19:39:47 +0200

    parametrize regex patterns tests

--- tests/test_regexes.py
+++ tests/test_regexes.py
@@ -1,25 +1,29 @@
-from trackie.conf import date_pattern
+import pytest
+
+from trackie.conf import (
+    date_pattern,
+)
 
 
-def test_check_format_invalid_date_month_13():
-    assert not date_pattern.match('2025-13-01\n')
+@pytest.mark.parametrize("date_string", [
+    '2025-13-01',
+    '205-12-01',
+    '2025-00-01',
+    '2025-1-01',
+    '2025-s1-01',
+    '2025-01-32',
+    '2025-01-00',
+])
+def test_invalid_date_patterns(date_string):
+    # assuming line breaks and trailing spaces are already stripped
+    assert not date_pattern.match(date_string)
 
 
-def test_check_format_invalid_date_month_two_digits():
-    assert not date_pattern.match('205-12-01\n')
-
-
-def test_check_format_invalid_date_month_00():
-    assert not date_pattern.match('2025-00-01')
-
-
-def test_check_format_invalid_date_month_1():
-    assert not date_pattern.match('2025-1-01')
-
-
-def test_check_format_invalid_date_month_contains_letter():
-    assert not date_pattern.match('2025-s1-01')
-
-
-def test_check_format_valid_date():
-    assert date_pattern.match('2025-01-01\n')
+@pytest.mark.parametrize("date_string", [
+    '2025-01-01',
+    '2025-12-31',
+    '2025-02-31',  # XXX todo: maybe we should catch this
+])
+def test_valid_date_patterns(date_string):
+    # assuming line breaks and trailing spaces are already stripped
+    assert date_pattern.match(date_string)
